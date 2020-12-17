38f85318332f0336587c18ccf38ee65409513dab
lukeEmano <lemano2@illinois.edu>
2020-12-17 12:04:34 -0600

    Baseline code

---
+++ eboard.js
@@ -0,0 +1,101 @@
+/*function togglePresident() {
+    var x = document.getElementById("presDescription");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function toggleEVP() {
+    var x = document.getElementById("EVPdescription");
+    var y = document.getElementById("AVPdescription");
+    if (x.style.display === "none" && y.style.display === "none") {
+        x.style.display = "block";
+    } else if (x.style.display === "none" && y.style.display === "block") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function toggleAVP() {
+    var x = document.getElementById("AVPdescription");
+    var y = document.getElementById("EVPdescription");
+    if (x.style.display === "none" && y.style.display === "none") {
+        x.style.display = "block";
+    } else if (x.style.display === "none" && y.style.display === "block") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function toggleIVP() {
+    var x = document.getElementById("IVPdescription");
+    var y = document.getElementById("VPFdescription");
+    if (x.style.display === "none" && y.style.display === "none") {
+        x.style.display = "block";
+    } else if (x.style.display === "none" && y.style.display === "block") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function toggleVPF() {
+    var x = document.getElementById("VPFdescription");
+    var y = document.getElementById("IVPdescription");
+    if (x.style.display === "none" && y.style.display === "none") {
+        x.style.display = "block";
+    } else if (x.style.display === "none" && y.style.display === "block") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function toggleVPC() {
+    var x = document.getElementById("VPCdescription");
+    var y = document.getElementById("VPNSdescription");
+    if (x.style.display === "none" && y.style.display === "none") {
+        x.style.display = "block";
+    } else if (x.style.display === "none" && y.style.display === "block") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function toggleVPNS() {
+    var x = document.getElementById("VPNSdescription");
+    var y = document.getElementById("VPCdescription");
+    if (x.style.display === "none" && y.style.display === "none") {
+        x.style.display = "block";
+    } else if (x.style.display === "none" && y.style.display === "block") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+*/
+
+$('.')
